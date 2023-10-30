bcf89047a6cbd7a6c51bf26125a7fe981b37804e
Willian <wsantos@ryndackcomponentes.com.br>
2023-10-30 11:25:13 -0300

    Primeiro Release

---
+++ Musica_R2R.c
@@ -0,0 +1,39 @@
+#define F_CPU 16000000
+
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include "Pontos.h"
+
+int i=0, Min=250;
+
+ISR(TIMER0_OVF_vect){							//Interrupção quando há um estouro do timer0.
+
+	TCNT0   = Min;								//Reinicia o timer0.
+
+	PORTD = pgm_read_byte_near(pontos + i);		//Manda vetor com o audio para a saida.
+	
+	if(i>998){				 				//incrmenta o vetor com o audio.
+		i =  0;
+	}
+	else{
+		i += 1;
+	}
+}
+
+void main (void){
+	SREG |= (1<<7);								//Habilita interruoções globais.
+	
+	TCCR0A  = 0;								//Configura o timer0 para operação normal, não conectada.
+	TCCR0B  = 0;
+	
+	TCCR0B |= ((1 << CS01)+(1 << CS00));		//Define o prescale como sendo 1/64.
+
+	TCNT0   = Min;								//Inicia o timer0.
+
+	TIMSK0 |= (1<<TOIE0);						//Habilita interrupção por estouro do timer 0.
+	
+	DDRD = 0xFF;								//Configura o PORTD (pinos 0 ao 7), como saídas.
+
+	while (1){	
+	}
+}
